ae4be40221ecc3828c70e0577fc899426a674a55
justGyga <iwantdrink229@gmail.com>
2023-12-07 15:01:41 +0700

    Date interval function

--- modules/jobs/receive_achievements.js
+++ modules/jobs/receive_achievements.js
@@ -1,5 +1,5 @@
-/* eslint-disable no-loop-func */
-/* eslint-disable guard-for-in */
+/* eslint-disable indent */
+/* eslint-disable default-case */
 import autoBind from "auto-bind";
 import { aggregateUserStatistic } from "../common/functions/get-statistic-by-time.js";
 import { ACHIEVEMENTS_REGIONAL_AFFLICTION, ACHIEVEMENTS_TIME_AFFLICTION } from "../models/achievements.js";
@@ -9,6 +9,32 @@ import User from "../models/user.js";
 class JobsWithAchievements {
     constructor() {
         autoBind(this);
+    }
+
+    async #setDateInterval(timeAffliction) {
+        // eslint-disable-next-line default-case
+        const endDate = new Date();
+        endDate.setDate(endDate.getDate() - 1);
+        endDate.setHours(23, 59, 59, 999);
+        const startDate = new Date(endDate);
+        switch (timeAffliction) {
+            case ACHIEVEMENTS_TIME_AFFLICTION.DAY:
+                startDate.setDate(endDate.getDate() - 1);
+                break;
+            case ACHIEVEMENTS_TIME_AFFLICTION.WEEK:
+                startDate.setDate(endDate.getDate() - 7);
+                break;
+            case ACHIEVEMENTS_TIME_AFFLICTION.MONTH:
+                startDate.setMonth(endDate.getMonth() - 6);
+                break;
+            case ACHIEVEMENTS_TIME_AFFLICTION.SEMI_ANNUAL:
+                startDate.setFullYear(endDate.getFullYear() - 1);
+                break;
+            case ACHIEVEMENTS_TIME_AFFLICTION.YEAR:
+                startDate.setFullYear(endDate.getFullYear() - 1);
+                break;
+        }
+        return { endDate, startDate };
     }
 
     async #putAchievements(statisticsForAwards, achievementMode, period) {
@@ -27,6 +53,7 @@ class JobsWithAchievements {
         delete statisticsForAwards.countryMaxPages;
         const locations = Object.values(ACHIEVEMENTS_REGIONAL_AFFLICTION);
         let index = 0;
+        // eslint-disable-next-line guard-for-in
         for (const regionalStatistic in statisticsForAwards) {
             const usersIds = [];
             Object.values(statisticsForAwards[regionalStatistic])
@@ -35,6 +62,7 @@ class JobsWithAchievements {
                     usersIds.push(statistic._id);
                 });
             const users = await User.find({ _id: { $in: usersIds } });
+            // eslint-disable-next-line no-loop-func
             users.forEach((user) => {
                 const userAchievementIndex = user.achievements[achievementMode].findIndex(
                     (achievement) => achievement.dateAffiliation == period && achievement.regionalAffiliation == locations[index]
@@ -86,11 +114,7 @@ class JobsWithAchievements {
     }
 
     async setDailyAchievements() {
-        const endDate = new Date();
-        endDate.setDate(endDate.getDate() - 1);
-        endDate.setHours(23, 59, 59, 999);
-        const startDate = new Date(endDate);
-        startDate.setDate(endDate.getDate() - 1);
+        const { endDate, startDate } = this.#setDateInterval(ACHIEVEMENTS_TIME_AFFLICTION.DAY);
         const aggregatedStatistics = await aggregateUserStatistic(startDate, endDate);
         const topsOfStatisticsSimpleMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.SIMPLE_MODE);
         const topsOfStatisticsWordMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.WORD_MODE);
@@ -99,11 +123,7 @@ class JobsWithAchievements {
     }
 
     async setWeeklyAchievements() {
-        const endDate = new Date();
-        endDate.setDate(endDate.getDate() - 1);
-        endDate.setHours(23, 59, 59, 999);
-        const startDate = new Date(endDate);
-        startDate.setDate(endDate.getDate() - 7);
+        const { endDate, startDate } = this.#setDateInterval(ACHIEVEMENTS_TIME_AFFLICTION.WEEK);
         const aggregatedStatistics = await aggregateUserStatistic(startDate, endDate);
         const topsOfStatisticsSimpleMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.SIMPLE_MODE);
         const topsOfStatisticsWordMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.WORD_MODE);
@@ -112,11 +132,7 @@ class JobsWithAchievements {
     }
 
     async setMonthlyAchievements() {
-        const endDate = new Date();
-        endDate.setDate(endDate.getDate() - 1);
-        endDate.setHours(23, 59, 59, 999);
-        const startDate = new Date(endDate);
-        startDate.setMonth(endDate.getMonth() - 1);
+        const { endDate, startDate } = this.#setDateInterval(ACHIEVEMENTS_TIME_AFFLICTION.MONTH);
         const aggregatedStatistics = await aggregateUserStatistic(startDate, endDate);
         const topsOfStatisticsSimpleMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.SIMPLE_MODE);
         const topsOfStatisticsWordMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.WORD_MODE);
@@ -125,11 +141,7 @@ class JobsWithAchievements {
     }
 
     async setSemiAnnualAchievements() {
-        const endDate = new Date();
-        endDate.setDate(endDate.getDate() - 1);
-        endDate.setHours(23, 59, 59, 999);
-        const startDate = new Date(endDate);
-        startDate.setMonth(endDate.getMonth() - 6);
+        const { endDate, startDate } = this.#setDateInterval(ACHIEVEMENTS_TIME_AFFLICTION.SEMI_ANNUAL);
         const aggregatedStatistics = await aggregateUserStatistic(startDate, endDate);
         const topsOfStatisticsSimpleMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.SIMPLE_MODE);
         const topsOfStatisticsWordMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.WORD_MODE);
@@ -138,11 +150,7 @@ class JobsWithAchievements {
     }
 
     async setYearAchievements() {
-        const endDate = new Date();
-        endDate.setDate(endDate.getDate() - 1);
-        endDate.setHours(23, 59, 59, 999);
-        const startDate = new Date(endDate);
-        startDate.setFullYear(endDate.getFullYear() - 1);
+        const { endDate, startDate } = this.#setDateInterval(ACHIEVEMENTS_TIME_AFFLICTION.YEAR);
         const aggregatedStatistics = await aggregateUserStatistic(startDate, endDate);
         const topsOfStatisticsSimpleMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.SIMPLE_MODE);
         const topsOfStatisticsWordMod = await this.#getTopOfStatistics(aggregatedStatistics, TOTAL_PAGES.WORD_MODE);
