596da9e8db2c133e8f568c0822f5e1d863f4cbf1
justGyga <iwantdrink229@gmail.com>
2024-03-19 22:48:52 +0700

    reamake geo for anonyms

--- modules/user/dto/geo-edit-dto.js
+++ modules/user/dto/geo-edit-dto.js
@@ -4,9 +4,3 @@ export const geoByCoordsDTO = yup.object().shape({
     latitude: yup.number().required(),
     longitude: yup.number().required()
 });
-
-export const geoByNamesDTO = yup.object().shape({
-    country: yup.string().required(),
-    city: yup.string().required(),
-    area: yup.string().required()
-});
